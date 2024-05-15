a880e9e98be3ca0bbe4482aa99231415a5edac2d
AgneseBeschin <165146408+AgneseBeschin@users.noreply.github.com>
2024-05-15 17:09:37 +0200

    Update AppTest.java

--- roman-number/src/test/java/it/unipd/mtss/AppTest.java
+++ roman-number/src/test/java/it/unipd/mtss/AppTest.java
@@ -1,35 +1,89 @@
+////////////////////////////////////////////////////////////////////
+// [Agnese] [Beschin] [2077995]
+// [Ivana Rose] [Setche] [2090030]
+////////////////////////////////////////////////////////////////////
+
+
 package it.unipd.mtss;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class AppTest extends TestCase {
+public class IntegerToRomanTest {
 
-    public AppTest(String testName) {
-        super(testName);
+    @Test
+    public void TestClasse_NewObject() {
+        IntegerToRoman object = new IntegerToRoman();
+        assertEquals(object.getClass(), IntegerToRoman.class);
     }
 
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
+    @Test
+    public void TestConvert_primi3Num() {
+        assertEquals(IntegerToRoman.convert(0),"");
+        assertEquals(IntegerToRoman.convert(-12),"");
+        assertEquals(IntegerToRoman.convert(1),"I");
+        assertEquals(IntegerToRoman.convert(2),"II");
+        assertEquals(IntegerToRoman.convert(3),"III");
     }
 
-    @Test 
-    // Test di conversione da numeri interi a numeri romani
-    public void testIntegerToRoman() {
-        assertEquals("I", IntegerToRoman.convert(1));
-        assertEquals("III", IntegerToRoman.convert(3));
-        assertEquals("IV", IntegerToRoman.convert(4));
-        assertEquals("V", IntegerToRoman.convert(5));
-        assertEquals("IX", IntegerToRoman.convert(9));
-        assertEquals("X", IntegerToRoman.convert(10));
-        assertEquals("XL", IntegerToRoman.convert(40));
-        assertEquals("L", IntegerToRoman.convert(50));
-        assertEquals("XC", IntegerToRoman.convert(90));
-        assertEquals("C", IntegerToRoman.convert(100));
-        assertEquals("CD", IntegerToRoman.convert(400));
-        assertEquals("D", IntegerToRoman.convert(500));
-        assertEquals("CM", IntegerToRoman.convert(900));
-        assertEquals("M", IntegerToRoman.convert(1000));
+    @Test
+    public void TestConvert_primi6Num() {
+        assertEquals(IntegerToRoman.convert(4),"IV");
+        assertEquals(IntegerToRoman.convert(5),"V");
+        assertEquals(IntegerToRoman.convert(6),"VI");
+    }
+
+    @Test
+    public void TestConvert_primi10Num() {
+        assertEquals(IntegerToRoman.convert(7),"VII");
+        assertEquals(IntegerToRoman.convert(8),"VIII");
+        assertEquals(IntegerToRoman.convert(9),"IX");
+        assertEquals(IntegerToRoman.convert(10),"X");
+    }
+
+    @Test
+    public void TestConvert_primi20Num() {
+        assertEquals(IntegerToRoman.convert(12),"XII");
+        assertEquals(IntegerToRoman.convert(13),"XIII");
+        assertEquals(IntegerToRoman.convert(15),"XV");
+        assertEquals(IntegerToRoman.convert(18),"XVIII");
+        assertEquals(IntegerToRoman.convert(19),"XIX");
+        assertEquals(IntegerToRoman.convert(20),"XX");
+    }
+
+    @Test
+    public void TestConvert_primi50Num() {
+        assertEquals(IntegerToRoman.convert(29),"XXIX");
+        assertEquals(IntegerToRoman.convert(35),"XXXV");
+        assertEquals(IntegerToRoman.convert(48),"XLVIII");
+        assertEquals(IntegerToRoman.convert(50),"L");
+    }
+
+    @Test
+    public void TestConvert_primi100Num() {
+        assertEquals(IntegerToRoman.convert(52),"LII");
+        assertEquals(IntegerToRoman.convert(77),"LXXVII");
+        assertEquals(IntegerToRoman.convert(85),"LXXXV");
+        assertEquals(IntegerToRoman.convert(93),"XCIII");
+        assertEquals(IntegerToRoman.convert(100),"C");
+    }
+
+    @Test
+    public void TestConvert_primi500Num(){
+        assertEquals(IntegerToRoman.convert(128),"CXXVIII");
+        assertEquals(IntegerToRoman.convert(200),"CC");
+        assertEquals(IntegerToRoman.convert(290),"CCXC");
+        assertEquals(IntegerToRoman.convert(318),"CCCXVIII");
+        assertEquals(IntegerToRoman.convert(420),"CDXX");
+        assertEquals(IntegerToRoman.convert(500),"D");
+    }
+
+    @Test
+    public void TestConvert_primi1000Num(){
+        assertEquals(IntegerToRoman.convert(628),"DCXXVIII");
+        assertEquals(IntegerToRoman.convert(795),"DCCXCV");
+        assertEquals(IntegerToRoman.convert(810),"DCCCX");
+        assertEquals(IntegerToRoman.convert(985),"CMLXXXV");
+        assertEquals(IntegerToRoman.convert(1000),"M");
     }
 }
